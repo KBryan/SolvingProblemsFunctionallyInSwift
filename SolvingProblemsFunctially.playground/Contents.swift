c5ce5a676385bc57c64a1f1ce698252a2c75306c
KBryan <kwame@3einteractive.com>
2015-11-08 10:29:14 -0500

    A functional example to solving programming problems

---
+++ SolvingProblemsFunctially.playground/Contents.swift
@@ -0,0 +1,58 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import Foundation
+
+func getContents(text:String)-> String {
+    let individualCharacter = text.componentsSeparatedByString(".")
+    var tempValue = ""
+    for (_,element)
+        in individualCharacter.enumerate(){
+        tempValue = "\(tempValue) \(element) \n"
+    }
+    return tempValue
+}
+getContents("a.n.t.o")
+func lines(input:String) ->[String] {
+    return input.componentsSeparatedByCharactersInSet(NSCharacterSet.newlineCharacterSet())
+}
+/// non-functional was of solving problem
+func nonFunctionalWay(input:String) -> Int {
+    let splittedByNs = getContents(input)
+    let convertedIntoArray = lines(splittedByNs)
+    return convertedIntoArray.count
+}
+
+nonFunctionalWay("K.w.a.m.e")
+/// getContents : String->String
+/// lines : String->[String]
+/// count : [String] -> Int
+/// getContents: String->String | lines : String -> [String] | count:[String] -> Int
+/// theFunctionalWay : String -> Int
+/// create a function on the fly
+var functionalWay = {
+    data in
+     return lines(getContents(data)).count
+}
+
+functionalWay("B.R.Y.A.N")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
